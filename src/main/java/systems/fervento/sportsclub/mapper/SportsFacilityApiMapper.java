539bbfcc3d9b4d4a2705988d24b4046c3f8486bc
Vincenzo Tramo <vv.tramo@gmail.com>
2022-10-30 22:08:32 +0100

    feat: add SportsFacilityApiMapper

---
+++ src/main/java/systems/fervento/sportsclub/mapper/SportsFacilityApiMapper.java
@@ -0,0 +1,16 @@
+package systems.fervento.sportsclub.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import systems.fervento.sportsclub.data.SportsFacilityData;
+import systems.fervento.sportsclub.openapi.model.SportsFacility;
+
+@Mapper(uses = AddressApiMapper.class)
+public interface SportsFacilityApiMapper {
+    SportsFacilityApiMapper INSTANCE = Mappers.getMapper(SportsFacilityApiMapper.class);
+
+    @Mapping(target = "totalSportsFields", expression = "java(sportsFacilityData.getSportsFields().size())")
+    @Mapping(target = "ownerId", source = "owner.id")
+    SportsFacility map(SportsFacilityData sportsFacilityData);
+}

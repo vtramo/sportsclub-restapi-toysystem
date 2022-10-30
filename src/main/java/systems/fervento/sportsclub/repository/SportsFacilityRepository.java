444277255ac449ab5361fe49849bd40e1455bf97
Vincenzo Tramo <vv.tramo@gmail.com>
2022-10-30 21:57:44 +0100

    feat: add sports facilities spring data jpa queries

--- src/main/java/systems/fervento/sportsclub/repository/SportsFacilityRepository.java
+++ src/main/java/systems/fervento/sportsclub/repository/SportsFacilityRepository.java
@@ -1,10 +1,19 @@
 package systems.fervento.sportsclub.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import systems.fervento.sportsclub.entity.SportsFacilityEntity;
+
+import java.util.List;
 
 @Repository
 public interface SportsFacilityRepository extends JpaRepository<SportsFacilityEntity, Long> {
 
+    List<SportsFacilityEntity> findAllByTotalNumberSportsFieldsBetween(@Param("min") int min, @Param("max") int max);
+    List<SportsFacilityEntity> findAllByOwnerIdAndTotalNumberSportsFieldsBetween(
+        @Param("ownerId") long ownerId,
+        @Param("min") int min,
+        @Param("max") int max
+    );
 }

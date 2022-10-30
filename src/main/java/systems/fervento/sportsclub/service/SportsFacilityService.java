27725ff5ace0bb010372c15f54c5aab56492ae26
Vincenzo Tramo <vv.tramo@gmail.com>
2022-10-30 21:58:55 +0100

    feat(service): implement /sports-facilities endpoint

--- src/main/java/systems/fervento/sportsclub/service/SportsFacilityService.java
+++ src/main/java/systems/fervento/sportsclub/service/SportsFacilityService.java
@@ -1,17 +1,44 @@
 package systems.fervento.sportsclub.service;
 
 import org.springframework.stereotype.Service;
+import systems.fervento.sportsclub.data.SportsFacilityData;
+import systems.fervento.sportsclub.mapper.SportsFacilityDataMapper;
 import systems.fervento.sportsclub.repository.SportsFacilityRepository;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toUnmodifiableList;
 
 @Service
 public class SportsFacilityService {
     private final SportsFacilityRepository sportsFacilityRepository;
 
+    private final SportsFacilityDataMapper sportsFacilityDataMapper = SportsFacilityDataMapper.INSTANCE;
+
     public SportsFacilityService(SportsFacilityRepository sportsFacilityRepository) {
         this.sportsFacilityRepository = sportsFacilityRepository;
     }
 
-    public Iterable<?> getAll() {
-        return sportsFacilityRepository.findAll();
+    public List<SportsFacilityData> getAllByTotalNumberSportsFieldBetween(
+        final int minTotalSportsFields,
+        final int maxTotalSportsFields
+    ) {
+        return sportsFacilityRepository
+                .findAllByTotalNumberSportsFieldsBetween(minTotalSportsFields, maxTotalSportsFields)
+                .stream()
+                .map(sportsFacilityDataMapper::map)
+                .collect(toUnmodifiableList());
+    }
+
+    public List<SportsFacilityData> getAllByOwnerIdAndTotalNumberSportsFieldBetween(
+        final int minTotalSportsFields,
+        final int maxTotalSportsFields,
+        final Long ownerId
+    ) {
+        return sportsFacilityRepository
+            .findAllByOwnerIdAndTotalNumberSportsFieldsBetween(ownerId, minTotalSportsFields, maxTotalSportsFields)
+            .stream()
+            .map(sportsFacilityDataMapper::map)
+            .collect(toUnmodifiableList());
     }
 }

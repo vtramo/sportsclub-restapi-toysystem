c536a7f49ba83432a143028b055a3001d99b3d45
Vincenzo Tramo <vv.tramo@gmail.com>
2022-10-30 21:53:55 +0100

    feat: add sports facilities named queries

--- src/main/java/systems/fervento/sportsclub/repository/package-info.java
+++ src/main/java/systems/fervento/sportsclub/repository/package-info.java
@@ -13,4 +13,41 @@
     }
 )
 
+/* Sports Facility Entity Queries */
+/*@NamedNativeQueries(value = {
+    @NamedNativeQuery(
+            name = "SportsFacilityEntity.findAllByTotalNumberSportsFieldsBetween",
+            query = "SELECT * FROM sports_facility_entity sf LEFT JOIN " +
+                    "(SELECT spf.sports_facility_id, COUNT(spf.sports_facility_id) AS tot " +
+                    "FROM sports_field_entity spf GROUP BY spf.sports_facility_id HAVING tot BETWEEN :min AND :max) AS sq " +
+                    "ON sf.id = sq.sports_facility_id WHERE COALESCE(sports_facility_id, 0) <> (CASE WHEN :min = 0 THEN -1 ELSE 0 END)",
+            resultClass = SportsFacilityEntity.class
+    ),
+    @NamedNativeQuery(
+        name = "SportsFacilityEntity.findAllByOwnerIdAndTotalNumberSportsFieldsBetween",
+        query = "SELECT * FROM sports_facility_entity sf LEFT JOIN " +
+                "(SELECT spf.sports_facility_id, COUNT(spf.sports_facility_id) AS tot " +
+                "FROM sports_field_entity spf GROUP BY spf.sports_facility_id HAVING tot BETWEEN :min AND :max) AS sq " +
+                "ON sf.id = sq.sports_facility_id " +
+                "WHERE COALESCE(sports_facility_id, 0) <> (CASE WHEN :min = 0 THEN -1 ELSE 0 END) " +
+                "AND owner_id = :ownerId",
+        resultClass = SportsFacilityEntity.class
+    )
+})*/
+
+@NamedQueries({
+    @NamedQuery(
+            name = "SportsFacilityEntity.findAllByTotalNumberSportsFieldsBetween",
+            query = "SELECT sf FROM SportsFacilityEntity sf WHERE sf.sportsFields.size > :min AND sf.sportsFields.size < :max"
+    ),
+    @NamedQuery(
+            name = "SportsFacilityEntity.findAllByOwnerIdAndTotalNumberSportsFieldsBetween",
+            query = "SELECT sf FROM SportsFacilityEntity sf WHERE sf.owner.id = :ownerId " +
+                    "AND sf.sportsFields.size > :min AND sf.sportsFields.size < :max"
+    )
+})
+
 package systems.fervento.sportsclub.repository;
+
+import org.hibernate.annotations.NamedQueries;
+import org.hibernate.annotations.NamedQuery;

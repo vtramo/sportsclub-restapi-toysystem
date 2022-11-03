f45e3fb922a8cdae8c1309334af76a9ed94ecf2b
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-03 22:18:24 +0100

    feat(mapper): add SportsFacilityEntityMapper

---
+++ src/main/java/systems/fervento/sportsclub/mapper/SportsFacilityEntityMapper.java
@@ -0,0 +1,20 @@
+package systems.fervento.sportsclub.mapper;
+
+import org.mapstruct.Context;
+import org.mapstruct.Mapper;
+import systems.fervento.sportsclub.data.SportsFacilityData;
+import systems.fervento.sportsclub.entity.SportsFacilityEntity;
+
+@Mapper(uses = {AddressEntityMapper.class, SportsFieldEntityMapper.class, UserEntityMapper.class})
+public interface SportsFacilityEntityMapper {
+
+    @DoIgnore
+    default SportsFacilityEntity mapToSportsFacilityEntity(SportsFacilityData sportsFieldData) {
+        return mapToSportsFacilityEntity(sportsFieldData, new CycleAvoidingMappingContext());
+    }
+
+    SportsFacilityEntity mapToSportsFacilityEntity(
+        SportsFacilityData sportsFacilityData,
+        @Context CycleAvoidingMappingContext cycleAvoidingMappingContext
+    );
+}

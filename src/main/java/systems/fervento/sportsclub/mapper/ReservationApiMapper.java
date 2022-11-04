4c8dcbe3788cbd73d83b8b31a2be3bc7d6696b12
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-04 14:41:50 +0100

    feat(mapper): add Page<NotificationData> to NotificationPage

--- src/main/java/systems/fervento/sportsclub/mapper/ReservationApiMapper.java
+++ src/main/java/systems/fervento/sportsclub/mapper/ReservationApiMapper.java
@@ -5,9 +5,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
+import org.springframework.data.domain.Page;
 import systems.fervento.sportsclub.data.ReservationData;
 import systems.fervento.sportsclub.openapi.model.DateRange;
 import systems.fervento.sportsclub.openapi.model.Reservation;
+import systems.fervento.sportsclub.openapi.model.ReservationPage;
+
+import static java.util.stream.Collectors.toList;
 
 @Mapper
 public interface ReservationApiMapper {
@@ -18,8 +22,35 @@ public interface ReservationApiMapper {
         final var dateRange = new DateRange();
         dateRange.setStartDate(reservationData.getStartDateTime());
         dateRange.setEndDate(reservationData.getEndDateTime());
+        reservation.setDateRange(dateRange);
     }
 
     @Mapping(target = "dateRange", ignore = true)
     Reservation mapToReservationApi(ReservationData reservationData);
+
+
+    default ReservationPage mapToReservationPage(Page<ReservationData> reservationDataPage) {
+        return mapToReservationPage(1, reservationDataPage);
+    }
+
+    @Mapping(target = "pageNo", expression = "java(notificationDataPage.getNumber())")
+    @Mapping(target = "pageSize", expression = "java(notificationDataPage.getSize())")
+    @Mapping(target = "totalPages", expression = "java(notificationDataPage.getTotalPages())")
+    @Mapping(target = "totalElements", expression = "java(notificationDataPage.getTotalElements())")
+    @Mapping(target = "last", expression = "java(notificationDataPage.isLast())")
+    @Mapping(target = "reservations", ignore = true)
+    @DoIgnore
+    ReservationPage mapToReservationPage(Integer dummy, Page<ReservationData> reservationDataPage);
+
+    @AfterMapping
+    default void setPageContentToNotificationPage(
+        @MappingTarget ReservationPage reservationData,
+        Page<ReservationData> reservationDataPage
+    ) {
+        reservationData.setReservations(reservationDataPage
+            .stream()
+            .map(this::mapToReservationApi)
+            .collect(toList())
+        );
+    }
 }

0ec12171ac959c096913ac22d0be5a84d8e131ea
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-04 11:32:34 +0100

    feat: add NotificationRepository

---
+++ src/main/java/systems/fervento/sportsclub/repository/NotificationRepository.java
@@ -0,0 +1,12 @@
+package systems.fervento.sportsclub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import systems.fervento.sportsclub.entity.NotificationEntity;
+
+import java.util.stream.Stream;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
+    Stream<NotificationEntity> findAllByOwnerId(Long ownerId);
+}

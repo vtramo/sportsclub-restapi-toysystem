bed4cc48911b925bc3554fcd7b2897247f181385
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-04 11:34:13 +0100

    feat(service): add getAllUserNotificationsByUserId service method

--- src/main/java/systems/fervento/sportsclub/service/UserService.java
+++ src/main/java/systems/fervento/sportsclub/service/UserService.java
@@ -1,22 +1,34 @@
 package systems.fervento.sportsclub.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import systems.fervento.sportsclub.data.NotificationData;
 import systems.fervento.sportsclub.data.UserData;
+import systems.fervento.sportsclub.entity.NotificationEntity;
+import systems.fervento.sportsclub.exception.ResourceNotFoundException;
+import systems.fervento.sportsclub.mapper.NotificationDataMapper;
 import systems.fervento.sportsclub.mapper.UserDataMapper;
 import systems.fervento.sportsclub.mapper.UserEntityMapper;
+import systems.fervento.sportsclub.repository.NotificationRepository;
 import systems.fervento.sportsclub.repository.UserRepository;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-
+    private final NotificationRepository notificationRepository;
+    private final NotificationDataMapper notificationDataMapper = NotificationDataMapper.INSTANCE;
     private final UserDataMapper userDataMapper = UserDataMapper.INSTANCE;
     private final UserEntityMapper userEntityMapper = UserEntityMapper.INSTANCE;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, NotificationRepository notificationRepository) {
         this.userRepository = userRepository;
+        this.notificationRepository = notificationRepository;
     }
 
     public UserData registerUser(UserData user) {
@@ -24,5 +36,18 @@ public class UserService {
         var userEntity = userEntityMapper.mapToUserEntity(user);
         userRepository.save(userEntity);
         return userDataMapper.map(userEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<NotificationData> getAllUserNotificationsByUserId(final long ownerId) {
+        if (!userRepository.existsById(ownerId)) {
+            throw new ResourceNotFoundException("The provided id doesn't identify any user!");
+        }
+
+        try (final Stream<NotificationEntity> notificationsStream = notificationRepository.findAllByOwnerId(ownerId)) {
+            return notificationsStream
+                .map(notificationDataMapper::mapToNotificationData)
+                .collect(toList());
+        }
     }
 }

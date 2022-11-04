d4bf73b50da6c77630b9e3214c400e359f019c75
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-04 14:18:22 +0100

    feat(repository): add NotificationEntity pageable queries to NotificationRepository

--- src/main/java/systems/fervento/sportsclub/repository/NotificationRepository.java
+++ src/main/java/systems/fervento/sportsclub/repository/NotificationRepository.java
@@ -1,13 +1,13 @@
 package systems.fervento.sportsclub.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import systems.fervento.sportsclub.entity.NotificationEntity;
 
-import java.util.stream.Stream;
-
 @Repository
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
-    Stream<NotificationEntity> findAllByOwnerId(Long ownerId);
-    Stream<NotificationEntity> findAllByOwnerIdAndHasBeenRead(Long ownerId, boolean hasBeenRead);
+    Page<NotificationEntity> findAllByOwnerId(Pageable pageable, Long ownerId);
+    Page<NotificationEntity> findAllByOwnerIdAndHasBeenRead(Pageable pageable, Long ownerId, boolean hasBeenRead);
 }

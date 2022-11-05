94fe5fed532e729540ec6557444118059d987189
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-05 16:50:56 +0100

    feat(service): add requestReservation service method to the ReservationService class

--- src/main/java/systems/fervento/sportsclub/service/ReservationService.java
+++ src/main/java/systems/fervento/sportsclub/service/ReservationService.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import systems.fervento.sportsclub.data.ReservationData;
-import systems.fervento.sportsclub.entity.ReservationStatus;
+import systems.fervento.sportsclub.entity.*;
+import systems.fervento.sportsclub.exception.ResourceNotFoundException;
 import systems.fervento.sportsclub.mapper.ReservationDataMapper;
 import systems.fervento.sportsclub.repository.ReservationRepository;
+import systems.fervento.sportsclub.repository.SportsFieldRepository;
+import systems.fervento.sportsclub.repository.UserRepository;
 
 import java.time.ZonedDateTime;
 import java.util.Objects;
@@ -16,31 +19,42 @@ import java.util.Optional;
 @Service
 public class ReservationService {
     private final ReservationRepository reservationRepository;
+    private final UserRepository userRepository;
+    private final SportsFieldRepository sportsFieldRepository;
     private final ReservationDataMapper reservationDataMapper = ReservationDataMapper.INSTANCE;
+    private final ReservationPriceCalculatorService reservationPriceCalculatorService;
 
-    public ReservationService(ReservationRepository reservationRepository) {
+    public ReservationService(
+        ReservationRepository reservationRepository,
+        UserRepository userService,
+        SportsFieldRepository sportsFieldService,
+        ReservationPriceCalculatorService reservationPriceCalculatorService
+    ) {
         this.reservationRepository = reservationRepository;
+        this.userRepository = userService;
+        this.sportsFieldRepository = sportsFieldService;
+        this.reservationPriceCalculatorService = reservationPriceCalculatorService;
     }
 
     public Page<ReservationData> getAllReservations(
-        int pageNo,
-        int pageSize,
-        Optional<String> optionalSortBy,
-        String state,
-        String sport,
-        Float price,
-        ZonedDateTime createdAt,
-        ZonedDateTime startDateTime,
-        ZonedDateTime endDateTime,
-        Long sportsFieldId,
-        Long sportsFacilityId
+        final int pageNo,
+        final int pageSize,
+        final Optional<String> optionalSortBy,
+        final String state,
+        final String sport,
+        final Float price,
+        final ZonedDateTime createdAt,
+        final ZonedDateTime startDateTime,
+        final ZonedDateTime endDateTime,
+        final Long sportsFieldId,
+        final Long sportsFacilityId
     ) {
         Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
 
         if (optionalSortBy.isPresent()) {
-            var sortingInfo = optionalSortBy.get().split("\\."); // example value: createdAt.asc
-            var sortingProperty = sortingInfo[0];
-            var sortDirection = Objects.equals(sortingInfo[1], "asc")
+            final var sortingInfo = optionalSortBy.get().split("\\."); // example value: createdAt.asc
+            final var sortingProperty = sortingInfo[0];
+            final var sortDirection = Objects.equals(sortingInfo[1], "asc")
                 ? Sort.Direction.DESC
                 : Sort.Direction.ASC;
             sort = Sort.by(sortDirection, sortingProperty);
@@ -59,5 +73,33 @@ public class ReservationService {
             sportsFieldId,
             sportsFacilityId
         ).map(reservationDataMapper::mapToReservationData);
+    }
+
+    public ReservationData requestReservation(final ReservationData reservationData) {
+        Objects.requireNonNull(reservationData);
+
+        final Long sportsFieldId = reservationData.getSportsFieldId();
+        final Long ownerId = reservationData.getOwnerId();
+        final ZonedDateTime startDateTime = reservationData.getStartDateTime();
+        final ZonedDateTime endDateTime   = reservationData.getEndDateTime();
+
+        final SportsFieldEntity sportsFieldEntity = sportsFieldRepository
+            .findById(sportsFieldId)
+            .orElseThrow(() -> new ResourceNotFoundException("There are no sports field with this ID!"));
+        final UserEntity userEntity = userRepository
+            .findById(ownerId)
+            .orElseThrow(() -> new ResourceNotFoundException("There are no user with this ID!"));
+
+        final var priceReservation = reservationPriceCalculatorService
+            .calculatePrice(startDateTime, endDateTime, sportsFieldEntity);
+
+        final var reservationEntity = new ReservationEntity();
+        reservationEntity.setPrice(priceReservation);
+        reservationEntity.setDateTimeRange(new DateTimeRange(startDateTime, endDateTime));
+        reservationEntity.setSportsField(sportsFieldEntity);
+        reservationEntity.setOwner(userEntity);
+        reservationRepository.save(reservationEntity);
+
+        return reservationDataMapper.mapToReservationData(reservationEntity);
     }
 }

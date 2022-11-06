d5a4c13c6d3bb80b32cf4b114ef927dec7895923
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-06 22:01:23 +0100

    feat: add setReservationState AfterMapping method in ReservationStatusApiMapper

--- src/main/java/systems/fervento/sportsclub/mapper/ReservationStatusApiMapper.java
+++ src/main/java/systems/fervento/sportsclub/mapper/ReservationStatusApiMapper.java
@@ -1,16 +1,24 @@
 package systems.fervento.sportsclub.mapper;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 import systems.fervento.sportsclub.data.ReservationData;
+import systems.fervento.sportsclub.openapi.model.ReservationStateEnum;
 import systems.fervento.sportsclub.openapi.model.ReservationStatus;
 
 @Mapper
 public interface ReservationStatusApiMapper {
     ReservationStatusApiMapper INSTANCE = Mappers.getMapper(ReservationStatusApiMapper.class);
 
+    @AfterMapping
+    default void setReservationState(@MappingTarget ReservationStatus reservationStatus, ReservationData reservationData) {
+        reservationStatus.setState(ReservationStateEnum.valueOf(reservationData.getState()));
+    }
+
     @Mapping(target = "reservationId", source = "id")
-    @Mapping(target = "state", expression = "java(ReservationStateEnum.valueOf(reservationData.getState()))")
+    @Mapping(target = "state", ignore = true)
     ReservationStatus mapToReservationStatus(ReservationData reservationData);
 }

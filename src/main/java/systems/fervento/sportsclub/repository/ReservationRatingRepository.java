dbe04d2207c91f4b2f9d718499b53e326d6415fa
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-06 22:12:52 +0100

    feat: add ReservationRatingRepository

---
+++ src/main/java/systems/fervento/sportsclub/repository/ReservationRatingRepository.java
@@ -0,0 +1,8 @@
+package systems.fervento.sportsclub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import systems.fervento.sportsclub.entity.ReservationRatingEntity;
+
+public interface ReservationRatingRepository extends JpaRepository<ReservationRatingEntity, Long> {
+
+}

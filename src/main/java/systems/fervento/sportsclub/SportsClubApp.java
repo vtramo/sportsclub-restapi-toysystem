995d4da5ac2508fcc2ddb3b5b7f9657e3cf1d4d4
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-06 22:01:41 +0100

    feat: enable scheduling app level

--- src/main/java/systems/fervento/sportsclub/SportsClubApp.java
+++ src/main/java/systems/fervento/sportsclub/SportsClubApp.java
@@ -2,10 +2,12 @@ package systems.fervento.sportsclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(
     scanBasePackages = "systems.fervento.sportsclub"
 )
+@EnableScheduling
 public class SportsClubApp {
     public static void main(String[] args) {
         SpringApplication.run(SportsClubApp.class, args);

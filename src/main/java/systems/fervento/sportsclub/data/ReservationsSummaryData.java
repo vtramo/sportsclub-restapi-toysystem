20e779683d779c7e1c5be372e6f9b7e7e4178831
Vincenzo Tramo <vv.tramo@gmail.com>
2022-11-07 18:04:36 +0100

    feat(data): add id property to ReservationsSummaryData

--- src/main/java/systems/fervento/sportsclub/data/ReservationsSummaryData.java
+++ src/main/java/systems/fervento/sportsclub/data/ReservationsSummaryData.java
@@ -11,6 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReservationsSummaryData {
+    private Long id;
     private ZonedDateTime createdAt;
     private String description;
     private ZonedDateTime startDateTime;

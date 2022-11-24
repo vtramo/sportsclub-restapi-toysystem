81ec1c8dbb00ca989e8d0388af04e38492fc7778
vtramo <vv.tramo@gmail.com>
2022-11-24 14:53:51 +0100

    feat: add SportEnumMapper as dependency to SportsReservationReportApiMapper

--- src/main/java/systems/fervento/sportsclub/mapper/SportsReservationReportApiMapper.java
+++ src/main/java/systems/fervento/sportsclub/mapper/SportsReservationReportApiMapper.java
@@ -4,7 +4,8 @@ import org.mapstruct.Mapper;
 import systems.fervento.sportsclub.data.SportsReservationReportData;
 import systems.fervento.sportsclub.openapi.model.SportsReservationReport;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = SportsEnumMapper.class)
 public interface SportsReservationReportApiMapper {
+
     SportsReservationReport mapToSportsReservationReport(SportsReservationReportData sportsReservationReportData);
 }

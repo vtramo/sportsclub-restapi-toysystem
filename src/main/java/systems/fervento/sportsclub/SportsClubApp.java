0849f354c6729052d1e575c7647b64071c6bd625
vtramo <vv.tramo@gmail.com>
2022-11-29 22:24:27 +0100

    feat: add initDatabase environment variable flag

--- src/main/java/systems/fervento/sportsclub/SportsClubApp.java
+++ src/main/java/systems/fervento/sportsclub/SportsClubApp.java
@@ -1,6 +1,7 @@
 package systems.fervento.sportsclub;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,27 +21,31 @@ import java.util.List;
 )
 @EnableScheduling
 public class SportsClubApp implements ApplicationRunner {
-
     @Autowired
     SportsFacilityRepository sportsFacilityRepository;
     @Autowired
     NotificationRepository notificationRepository;
     @Autowired
     ReservationRepository reservationRepository;
-
     @Autowired
     UserRepository userRepository;
-    Address address;
-    UserEntity userEntity;
-    SportsFieldEntity sportsFieldEntity1, sportsFieldEntity2, sportsFieldEntity3;
-    SportsFacilityEntity sportsFacilityEntity1, sportsFacilityEntity2;
-    SportsFieldPriceListEntity sportsFieldPriceListEntity;
-    CreditCardEntity creditCardEntity;
-    List<NotificationEntity> notificationsUserEntity1;
-    List<ReservationEntity> reservationsUserEntity1;
 
-    void setupDatabase() {
-        address = new Address();
+    @Value(value = "${INIT_DATABASE:false}")
+    private String initDatabase;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsClubApp.class, args);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (Boolean.parseBoolean(initDatabase)) {
+            initDatabase();
+        }
+    }
+
+    void initDatabase() {
+        Address address = new Address();
         address.setStreetNumber("32");
         address.setStreetName("Via Vittorio Bachelet");
         City city = new City();
@@ -49,7 +54,7 @@ public class SportsClubApp implements ApplicationRunner {
         city.setPostalCode("80021");
         address.setCity(city);
 
-        userEntity = new UserEntity();
+        UserEntity userEntity = new UserEntity();
         userEntity.setUsername("bonek");
         userEntity.setPassword("ciao");
         userEntity.setFirstName("Vincenzo");
@@ -58,46 +63,49 @@ public class SportsClubApp implements ApplicationRunner {
         userEntity.setEmail("vv.tramo@gmail.com");
         userEntity.setHomeAddress(address);
 
-        creditCardEntity = new CreditCardEntity("AAAAAAAAAAAAAAAA", "BBBBB", "CCCC");
+        CreditCardEntity creditCardEntity = new CreditCardEntity("AAAAAAAAAAAAAAAA", "BBBBB", "CCCC");
         userEntity.addBillingDetails(creditCardEntity);
         creditCardEntity.setOwner(userEntity);
 
-        sportsFacilityEntity1 = new SportsFacilityEntity("Sports Club 2022", "666");
+        SportsFacilityEntity sportsFacilityEntity1 = new SportsFacilityEntity("Sports Club 2022", "666");
         sportsFacilityEntity1.setAddress(address);
         userEntity.addSportsFacility(sportsFacilityEntity1);
         sportsFacilityEntity1.setOwner(userEntity);
 
-        sportsFacilityEntity2 = new SportsFacilityEntity("New Sports Club 2022", "777");
+        SportsFacilityEntity sportsFacilityEntity2 = new SportsFacilityEntity("New Sports Club 2022", "777");
         sportsFacilityEntity2.setAddress(address);
         sportsFacilityEntity2.setOwner(userEntity);
 
-        sportsFieldEntity1 = new SoccerFieldEntity("Eden", SoccerFieldType.ELEVEN_A_SIDE, true);
-        sportsFieldEntity2 = new BasketballFieldEntity("Basket Field", false);
-        sportsFieldEntity3 = new TennisFieldEntity("Tennis Field", TennisFieldType.CEMENT, false);
+        SportsFieldEntity sportsFieldEntity1 = new SoccerFieldEntity("Eden", SoccerFieldType.ELEVEN_A_SIDE, true);
+        SportsFieldEntity sportsFieldEntity2 = new BasketballFieldEntity("Basket Field", false);
+        SportsFieldEntity sportsFieldEntity3 = new TennisFieldEntity("Tennis Field", TennisFieldType.CEMENT, false);
         sportsFieldEntity1.setSportsFacility(sportsFacilityEntity1);
+        sportsFieldEntity1.setPriceList(new SportsFieldPriceListEntity(0, 0));
         sportsFieldEntity2.setSportsFacility(sportsFacilityEntity2);
+        sportsFieldEntity2.setPriceList(new SportsFieldPriceListEntity(0, 0));
         sportsFieldEntity3.setSportsFacility(sportsFacilityEntity2);
+        sportsFieldEntity3.setPriceList(new SportsFieldPriceListEntity(0, 0));
 
         sportsFacilityEntity1.getSportsFields().add(sportsFieldEntity1);
         sportsFacilityEntity2.getSportsFields().add(sportsFieldEntity2);
         sportsFacilityEntity2.getSportsFields().add(sportsFieldEntity3);
 
-        sportsFieldPriceListEntity = new SportsFieldPriceListEntity(75.0f, 5.0f);
+        SportsFieldPriceListEntity sportsFieldPriceListEntity = new SportsFieldPriceListEntity(75.0f, 5.0f);
         sportsFieldEntity1.setPriceList(sportsFieldPriceListEntity);
 
-        notificationsUserEntity1 = List.of(
-                new NotificationEntity(null, ZonedDateTime.now(), true, "ciao", userEntity),
-                new NotificationEntity(null, ZonedDateTime.now(), false, "ciao2", userEntity),
-                new NotificationEntity(null, ZonedDateTime.now(), false, "ciao3", userEntity)
+        List<NotificationEntity> notificationsUserEntity1 = List.of(
+            new NotificationEntity(null, ZonedDateTime.now(), true, "ciao", userEntity),
+            new NotificationEntity(null, ZonedDateTime.now(), false, "ciao2", userEntity),
+            new NotificationEntity(null, ZonedDateTime.now(), false, "ciao3", userEntity)
         );
 
         var dateTimeRange = new DateTimeRange(ZonedDateTime.now(), ZonedDateTime.now().plusDays(1));
         var dateTimeRange2 = new DateTimeRange(ZonedDateTime.now().minusDays(3), ZonedDateTime.now().minusDays(2));
-        reservationsUserEntity1 = List.of(
-                new ReservationEntity(dateTimeRange, 10f, userEntity),
-                new ReservationEntity(dateTimeRange, 10f, userEntity),
-                new ReservationEntity(dateTimeRange, 10f, userEntity),
-                new ReservationEntity(dateTimeRange2, 30f, userEntity)
+        List<ReservationEntity> reservationsUserEntity1 = List.of(
+            new ReservationEntity(dateTimeRange, 10f, userEntity),
+            new ReservationEntity(dateTimeRange, 10f, userEntity),
+            new ReservationEntity(dateTimeRange, 10f, userEntity),
+            new ReservationEntity(dateTimeRange2, 30f, userEntity)
         );
         reservationsUserEntity1.get(0).setSportsField(sportsFieldEntity1);
         reservationsUserEntity1.get(1).setSportsField(sportsFieldEntity1);
@@ -127,14 +135,5 @@ public class SportsClubApp implements ApplicationRunner {
         sportsFacilityRepository.save(sportsFacilityEntity2);
         sportsFacilityRepository.save(sportsFacilityEntity1);
         reservationRepository.saveAll(reservationsUserEntity1);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(SportsClubApp.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        setupDatabase();
     }
 }

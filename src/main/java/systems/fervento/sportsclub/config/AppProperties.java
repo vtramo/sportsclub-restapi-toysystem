85e58e526811fd50ef4b2307f80f2f5e653868a7
vtramo <vv.tramo@gmail.com>
2022-12-01 21:58:12 +0100

    fix: add @Configuration annotation to AppProperties

--- src/main/java/systems/fervento/sportsclub/config/AppProperties.java
+++ src/main/java/systems/fervento/sportsclub/config/AppProperties.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotNull;
 
+@Configuration
 @ConfigurationProperties(prefix = "app.config")
 @Validated // Necessario per validazione!app.config
 @NoArgsConstructor
